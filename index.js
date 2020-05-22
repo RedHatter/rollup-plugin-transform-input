94e5f7c895caa838b57f09c50b5d1f8aa5d7aaf0
Timothy Johnson <timothy@idioticdev.com>
2020-05-21 19:00:35 -0700

    Formatting

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-import { promises as fs } from "fs";
+import { promises as fs } from 'fs'
 import { relative } from 'path'
-import { createFilter } from "@rollup/pluginutils";
+import { createFilter } from '@rollup/pluginutils'
 
 export default function (options = {}) {
   const {
     include,
-    exclude = "node_modules/**",
+    exclude = 'node_modules/**',
     silent = false,
-    transform
-  } = options;
+    transform,
+  } = options
 
-  if (typeof transform != 'function') throw new TypeError('The transform option must be a function')
+  if (typeof transform != 'function')
+    throw new TypeError('The transform option must be a function')
 
-  const filter = createFilter(include, exclude);
+  const filter = createFilter(include, exclude)
   const cwd = process.cwd()
 
   return {
-    name: "input-prettier",
+    name: 'transform-input',
     async transform(source, id) {
-      if (!filter(id)) return null;
+      if (!filter(id)) return null
 
       const start = process.hrtime.bigint()
-      const formatted = await transform(source, id);
+      const formatted = await transform(source, id)
 
       if (formatted != source) {
-        await fs.writeFile(id, formatted);
+        await fs.writeFile(id, formatted)
         if (silent) return null
 
         const diff = process.hrtime.bigint() - start
-        const time = (diff - diff % 1000000n) / 1000000n
-        console.log(`${relative(cwd, id)} ${time}ms`);
+        const time = (diff - (diff % 1000000n)) / 1000000n
+        console.log(`${relative(cwd, id)} ${time}ms`)
       }
 
-      return null;
+      return null
     },
-  };
+  }
 }
