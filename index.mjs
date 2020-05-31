ade2675803ba1079cb6790d2213bc17bbff57bee
RedHatter <ava.johnson@zohomail.com>
2020-05-31 06:56:57 -0700

    Option to load source

--- index.mjs
+++ index.mjs
@@ -7,6 +7,7 @@ export default function (options = {}) {
     include,
     exclude = 'node_modules/**',
     silent = false,
+    load = false,
     transform,
   } = options
 
@@ -16,23 +17,33 @@ export default function (options = {}) {
   const filter = createFilter(include, exclude)
   const cwd = process.cwd()
 
+  async function transformSource (source, id) {
+    const start = process.hrtime.bigint()
+    const formatted = await transform(source, id)
+
+    if (formatted != source) {
+      await fs.writeFile(id, formatted)
+      if (silent) return null
+
+      const diff = process.hrtime.bigint() - start
+      const time = (diff - (diff % 1000000n)) / 1000000n
+      console.log(`${relative(cwd, id)} ${time}ms`)
+    }
+  }
+
   return {
     name: 'transform-input',
+    async load (id) {
+      if (!load || !filter(id)) return null
+
+      await transformSource(await fs.readFile(id, 'utf8'), id)
+      return null
+    },
+
     async transform(source, id) {
-      if (!filter(id)) return null
+      if (load || !filter(id)) return null
 
-      const start = process.hrtime.bigint()
-      const formatted = await transform(source, id)
-
-      if (formatted != source) {
-        await fs.writeFile(id, formatted)
-        if (silent) return null
-
-        const diff = process.hrtime.bigint() - start
-        const time = (diff - (diff % 1000000n)) / 1000000n
-        console.log(`${relative(cwd, id)} ${time}ms`)
-      }
-
+      await transformSource(source, id)
       return null
     },
   }
